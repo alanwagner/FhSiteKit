5e2bdf4c5a49c19d3da47479e93b0f600894c9fa
alanwagner <mail@alanwagner.org>
2013-10-29 12:13:36 +0100

    Added FhSiteKit\AbstractModule!

---
+++ FhskCore/src/FhSiteKit/AbstractModule.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Farther Horizon Site Kit
+ *
+ * @link       http://github.com/alanwagner/FhSiteKit for the canonical source repository
+ * @copyright Copyright (c) 2013 Farther Horizon SARL (http://www.fartherhorizon.com)
+ * @license   http://www.gnu.org/licenses/gpl-3.0.html GPLv3 License
+ * @author    Alan Wagner (mail@alanwagner.org)
+ */
+
+namespace FhSiteKit;
+
+use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
+use Zend\ModuleManager\Feature\ConfigProviderInterface;
+use Zend\ModuleManager\Feature\DependencyIndicatorInterface;
+
+class AbstractModule implements AutoloaderProviderInterface, ConfigProviderInterface, DependencyIndicatorInterface
+{
+    /**
+     * Get module config
+     * @return array
+     */
+    public function getConfig()
+    {
+        return array();
+    }
+
+    /**
+     * Get module autoloader config
+     * @return array
+     */
+    public function getAutoloaderConfig()
+    {
+        return array();
+    }
+
+    /**
+     * Expected to return an array of modules on which the current one depends on
+     *
+     * @return array
+     */
+    public function getModuleDependencies()
+    {
+        return array();
+    }
+}
