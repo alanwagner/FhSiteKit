c26bfe3e243a426c714e3825d7039dcd7256bc2e
alanwagner <mail@alanwagner.org>
2013-11-18 15:19:39 +0100

    Updated phpunit.sh

--- bin/phpunit.sh
+++ bin/phpunit.sh
@@ -3,6 +3,11 @@ if [ ! -d ../ZF2/library/Zend ]; then
   exit
 fi
 
-phpunit  -c FhskCore/test;
-phpunit -c FhskConfig/test;
+if [ ! -f ../phpunit/phpunit/phpunit.php ]; then
+  echo "This is written to be run parallel to a phpunit/phpunit vendor directory"
+  exit
+fi
+
+../phpunit/phpunit/phpunit.php -c FhskCore/test;
+../phpunit/phpunit/phpunit.php -c FhskConfig/test;
 
